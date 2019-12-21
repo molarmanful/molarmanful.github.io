c94e29ffc6f96e3960a8b234be2761807e511a88
Ben Pang <benjinpang@gmail.com>
2019-12-22 00:16:30 +0400

    important fixes to codebase

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 $(_=>{
-  let cl = x=>{
-    [['.l','L'],['.lv','Lv'],['.lp','Lp'],['.lpv','Lpv'],['.lpV','LpV']].map(a=>{
-      $('.B').eq(x).find(a[0]).addClass(a[1])
-    })
-  }
 
   $('body').fadeIn(1000, _=>{
 
@@ -11,18 +6,13 @@ $(_=>{
       licenseKey: '0BD90318-FEBE4A0A-AB35E65A-31E4D65D',
       sectionSelector:'.B',
       verticalCentered: false,
-      scrollingSpeed: 500,
       onLeave: (x,y)=>{
         $('body').attr('class', 'b'+y.index)
-        $('.L,.Lv,.Lp,.Lpv,.LpV').removeClass('L Lv Lp Lpv LpV')
         let occ = 0
         $('#side').css({
           color: $('.B').eq(y.index).css('color')
         })
         $('.nav').html('□').eq(y.index).html('■')
-      },
-      afterLoad: (x,y)=>{
-        cl(y.index)
       }
     })
 
@@ -30,7 +20,6 @@ $(_=>{
     $('.sd').click(_=> $.fn.fullpage.moveSectionDown())
     $('.nav').click(e=> $.fn.fullpage.moveTo([...e.currentTarget.parentElement.children].indexOf(e.currentTarget)+1))
 
-    cl(0)
     $('#side').css('color', $('.B').eq(0).css('color'))
 
   })
