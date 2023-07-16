b357526b4fa54c903f8527223a31477532ff8f28
molarmanful <benjinpang@gmail.com>
2023-07-15 20:30:47 -0400

    better images

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite'
 
 export default {
   plugins: [
-    imagetools({
-      include: /^[^?]+\.(heic|heif|avif|jpeg|jpg|png|tiff|webp|gif|x)(\?.*)?$/,
-    }),
+    imagetools(),
     unocss(),
     sveltekit(),
   ],
