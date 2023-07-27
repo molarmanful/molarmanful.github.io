057d4bb307ea581782162d6eada2660032eae8d5
molarmanful <benjinpang@gmail.com>
2023-07-27 12:01:54 -0400

    dang

--- src/lib/mixins/Section.svelte
+++ src/lib/mixins/Section.svelte
@@ -1,7 +1,7 @@
 <script>
   import { OffTop } from '.'
 
-  export let name = ''
+  export let name
   export let bord = false
   export let ot = 0
 </script>
@@ -18,26 +18,28 @@
     {...$$restProps}
   >
     <div data-aos="fade-right" flex="xl:~ 1 col" hidden>
-      <div ml-auto p="t-6 b-32 xl:x-4" sticky top-0 type-7 write-vertical-left>
-        <h1
-          inline-block
-          origin-center
-          role={name ? void 0 : 'none'}
-          rotate-180
-          select-none
-          text="stroked gray-500"
+      {#if name}
+        <div
+          ml-auto
+          p="t-6 b-32 xl:x-4"
+          sticky
+          top-0
+          type-7
+          write-vertical-left
         >
-          {name}
-        </h1>
-      </div>
+          <h1
+            inline-block
+            origin-center
+            rotate-180
+            select-none
+            text="stroked gray-500"
+          >
+            {name}
+          </h1>
+        </div>
+      {/if}
     </div>
     <slot />
-    <div border={bord ? 't gray-500' : void 0} flex="xl:~ 1" hidden>
-      <div mr-auto p="t-6 b-32 xl:x-4" top-0 type-7 write-vertical-left>
-        <h1 inline-block origin-center role="none" rotate-180 select-none>
-          &nbsp;
-        </h1>
-      </div>
-    </div>
+    <div border={bord ? 't gray-500' : void 0} flex="xl:~ 1" hidden></div>
   </section>
 </OffTop>
