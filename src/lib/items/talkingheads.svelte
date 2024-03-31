b16fab82f04e35982f03ee847a255914e6f19cdf
molarmanful <benjinpang@gmail.com>
2024-03-31 03:00:20 -0400

    talking-heads

---
+++ src/lib/items/talkingheads.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { A, Heading, MediaImg, Prose, Subheading, Text } from '../mixins'
+
+  export const desc =
+    'An experimental chatroom that enables mere mortals to communicate with the gods.'
+</script>
+
+<Prose>
+  <Heading>TALKING-HEADS</Heading>
+  <Subheading>{desc}</Subheading>
+  <Text>
+    <A href="https://talking-heads.fly.dev">Website</A>
+    <br />
+    <A href="https://github.com/molarmanful/talking-heads">Source</A>
+  </Text>
+  <MediaImg name="talking-heads" />
+</Prose>
