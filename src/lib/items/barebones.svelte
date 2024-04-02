c86de3e5f20269925de449053b69c6f0d0181913
molarmanful <benjinpang@gmail.com>
2024-04-02 13:38:46 -0400

    barebones

---
+++ src/lib/items/barebones.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { A, Heading, MediaImg, Prose, Subheading, Text } from '../mixins'
+
+  export const desc =
+    'A web art piece centered around the premise of an anti-social, self-destructive website.'
+</script>
+
+<Prose>
+  <Heading>BAREBONES</Heading>
+  <Subheading>{desc}</Subheading>
+  <Text>
+    <A href="https://bennyboy.tech/barebones">Website</A>
+    <br />
+    <A href="https://github.com/molarmanful/barebones">Source</A>
+  </Text>
+  <MediaImg name="barebones" />
+</Prose>
