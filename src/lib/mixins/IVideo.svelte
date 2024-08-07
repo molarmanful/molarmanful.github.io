8bbac5f68a3f65932bf4145dadb4a7cf5ed0157d
molarmanful <benjinpang@gmail.com>
2024-08-06 20:42:50 -0400

    hmmm

--- src/lib/mixins/IVideo.svelte
+++ src/lib/mixins/IVideo.svelte
@@ -6,8 +6,8 @@
 
 <Video {aspect}>
   <div class="aspect-{aspect}" max-full mx-auto>
-    <video autoplay full loading="lazy" loop muted object-contain playsinline>
-      <source src={`https://i.imgur.com/${a}.mp4`} type="video/mp4" />
+    <video autoplay full loop muted object-contain playsinline>
+      <source src="https://i.imgur.com/{a}.mp4" type="video/mp4" />
     </video>
   </div>
 </Video>
