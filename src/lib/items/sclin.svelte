207cab8ba00bf2559a22fac494afb1ab1d0989a6
molarmanful <benjinpang@gmail.com>
2024-08-11 20:49:22 -0400

    more alt text

--- src/lib/items/sclin.svelte
+++ src/lib/items/sclin.svelte
@@ -1,6 +1,6 @@
 <script context="module">
   export const desc =
-    '2024. Functional and array programming paradigms packaged into a stack-based language. Tuned for maximum expressiveness.'
+    '2024. Functional and array programming paradigms packaged into a stack-based language. Tuned for maximum expressiveness. Features a web-based interpreter and tree-sitter grammar.'
   export const tags = [
     'digital',
     'dev',
@@ -27,5 +27,16 @@
   </Text>
 
   <Sp />
-  <!-- <MediaImg name="" /> -->
+  <MediaImg
+    name="sclin_code"
+    alt="sclin code for generating an ASCII Mandelbrot set."
+  />
+  <MediaImg
+    name="sclin_mandelbrot"
+    alt="Terminal output of the sclin ASCII Mandelbrot generator."
+  />
+  <MediaImg
+    name="scline"
+    alt="Web interface of scline, the online interpreter for sclin."
+  />
 </Prose>
--- src/lib/items/vector.svelte
+++ src/lib/items/vector.svelte
@@ -6,6 +6,7 @@
 </script>
 
 <script>
+  import { artalt } from '../js/util.svelte'
   import { ArtImg, Heading, Prose, Subheading } from '../mixins'
 </script>
 
@@ -14,25 +15,25 @@
   <Subheading>{desc}</Subheading>
 </Prose>
 
-<ArtImg name="plane" />
-<ArtImg name="meltdown" />
-<ArtImg name="bang" />
-<ArtImg name="crow" />
-<ArtImg name="beetle" />
-<ArtImg name="jellyfish" />
-<ArtImg name="somanyhands" />
-<ArtImg name="deepspider" />
-<ArtImg name="botfight" />
-<ArtImg name="tpdneoldman" />
-<ArtImg name="somerandomwoman" />
-<ArtImg name="woman" />
-<ArtImg name="synthwoman" />
-<ArtImg name="haringrun" />
-<ArtImg name="tvdude" />
-<ArtImg name="snowman" />
-<ArtImg name="lama" />
-<ArtImg name="cobra" />
-<ArtImg name="hand2" />
-<ArtImg name="cake" />
-<ArtImg name="weirdface" />
-<ArtImg name="freefall" />
+<ArtImg name="plane" alt={artalt('SEABIRD')} />
+<ArtImg name="meltdown" alt={artalt('MELTDOWN')} />
+<ArtImg name="bang" alt={artalt('HAND GUN')} />
+<ArtImg name="crow" alt={artalt('THE JACKDAW FLIES')} />
+<ArtImg name="beetle" alt={artalt('DOGBANE BEETLE')} />
+<ArtImg name="jellyfish" alt={artalt('JELLYFISH')} />
+<ArtImg name="somanyhands" alt={artalt('SO MANY HANDS')} />
+<ArtImg name="deepspider" alt={artalt('DEEP SEA SPIDER')} />
+<ArtImg name="botfight" alt={artalt('BOTFIGHT')} />
+<ArtImg name="tpdneoldman" alt={artalt('SOME OLD MAN')} />
+<ArtImg name="somerandomwoman" alt={artalt('SOME RANDOM WOMAN')} />
+<ArtImg name="woman" alt={artalt('SOME WOMAN')} />
+<ArtImg name="synthwoman" alt={artalt('SOME SYNTH')} />
+<ArtImg name="haringrun" alt={artalt('WEE WOO')} />
+<ArtImg name="tvdude" alt={artalt('TV DUDE')} />
+<ArtImg name="snowman" alt={artalt('BLIND MAN TRIPS THROUGH THE SNOW')} />
+<ArtImg name="lama" alt={artalt('LAMA 95')} />
+<ArtImg name="cobra" alt={artalt('COBRA')} />
+<ArtImg name="hand2" alt={artalt('HAND')} />
+<ArtImg name="cake" alt={artalt('CAKE DAY')} />
+<ArtImg name="weirdface" alt={artalt('WEIRD FACE')} />
+<ArtImg name="freefall" alt={artalt('FREEFALL ETUDE')} />
