cfb214ba7a20073a6e64db27310f233676f2a6f3
molarmanful <benjinpang@gmail.com>
2024-08-16 19:27:33 -0400

    more subtle

--- src/lib/mixins/HeaderSplash.svelte
+++ src/lib/mixins/HeaderSplash.svelte
@@ -1,12 +1,22 @@
 <script>
   import me from '../js/me'
+  import { redmote, sfactor } from '../js/util.svelte'
 
   import { SvgEl } from '.'
 
   let { ...props } = $props()
+
+  let factor = $state(0)
+  sfactor(x => (factor = x))
+
+  const rm = redmote()
 </script>
 
-<svg viewBox="0 0 1587 1080" {...props}>
+<svg
+  style:transform={rm ? `scale(${1 + 0.1 * Math.min(1, factor)})` : void 0}
+  viewBox="0 0 1587 1080"
+  {...props}
+>
   {#each me as el}
     <SvgEl name={el.this} len={el.len} {...el.attr} />
   {/each}
