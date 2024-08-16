9ee0f3fd6ad1b861c18c30f04abcded13372b450
molarmanful <benjinpang@gmail.com>
2024-08-16 19:19:33 -0400

    Revert 1 commits
    28644aa 'trying something really weird'

--- src/lib/mixins/HeaderSplash.svelte
+++ src/lib/mixins/HeaderSplash.svelte
@@ -1,22 +1,12 @@
 <script>
   import me from '../js/me'
-  import { redmote, sfactor } from '../js/util.svelte'
 
   import { SvgEl } from '.'
 
-  let factor = $state(0)
-  sfactor(x => (factor = x))
-
   let { ...props } = $props()
-
-  const rm = redmote()
 </script>
 
-<svg
-  style:transform={rm ? `scale(${1 + factor * 0.1})` : void 0}
-  viewBox="0 0 1587 1080"
-  {...props}
->
+<svg viewBox="0 0 1587 1080" {...props}>
   {#each me as el}
     <SvgEl name={el.this} len={el.len} {...el.attr} />
   {/each}
