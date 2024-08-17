e5fa68ae31ab6205485218d6edade4c5e27d2975
molarmanful <benjinpang@gmail.com>
2024-08-17 17:05:01 -0400

    reimplement header mouse effect

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,16 +1,54 @@
 <script>
-  import { fadeonly, sfactor } from './js/util.svelte'
+  import { fadeonly, redmote, sfactor } from './js/util.svelte'
   import { HeaderSplash, HeaderTitle } from './mixins'
 
   let { top = () => {} } = $props()
 
   let factor = $state(0)
+  const rm = redmote()
+  const fo = fadeonly()
+  const fac_inv = $derived(Math.max(0, 1 - factor))
   sfactor(x => (factor = x))
 
-  const fo = fadeonly()
+  let innerWidth = $state(0)
+  let innerHeight = $state(0)
+  const mouse = $state({ x: 0, y: 0 })
+  const mouse_rel = $derived(
+    rm.matches || factor >= 1 ?
+      { x: 0, y: 0 }
+    : {
+        x: (mouse.x / innerWidth - 1) * fac_inv,
+        y: (mouse.y / innerHeight - 1) * fac_inv,
+      }
+  )
+  const scale = $derived(rm.matches || 1 + 0.1 * fac_inv)
+
+  const splash_rel = $derived({
+    x: -1.5 * mouse_rel.x,
+    y: -1.5 * mouse_rel.y,
+  })
+  const title_rel = $derived({
+    x: mouse_rel.x,
+    y: mouse_rel.y - !fo.matches * factor * 10,
+  })
+
+  $effect(() => {
+    if (!fo) return
+    mouse.x = innerWidth / 2
+    mouse.y = innerHeight / 2
+  })
 
   top('top')
 </script>
+
+<svelte:window
+  onmousemove={({ clientX, clientY }) => {
+    mouse.x = clientX
+    mouse.y = clientY
+  }}
+  bind:innerWidth
+  bind:innerHeight
+/>
 
 <header id="top" relative screen>
   <div style:filter="hue-rotate({factor * -69}deg)" fixed flex full>
@@ -19,12 +57,16 @@
       h="lvh"
       mx="auto"
       right="0 media-squarish:40%"
+      {scale}
+      {splash_rel}
       top="0"
       un-transform="~ translate-x-[var(--t-x,0%)] media-squarish:translate-x-[calc(50%+var(--t-x,0%))]"
     />
     <div
       style:opacity={1 - factor * 4}
-      style:transform={fo.matches ? 'none' : `translateY(-${factor * 10}%)`}
+      style:transform="translateX({title_rel.x}%) translateY({title_rel.y}%)"
+      absolute
+      inset-0
     >
       <HeaderTitle />
     </div>
--- src/lib/mixins/HeaderSplash.svelte
+++ src/lib/mixins/HeaderSplash.svelte
@@ -1,45 +1,16 @@
 <script>
   import me from '../js/me'
-  import { redmote, sfactor } from '../js/util.svelte'
 
   import { SvgEl } from '.'
 
-  let { ...props } = $props()
-
-  let factor = $state(0)
-  const rm = redmote()
-  const fac_inv = $derived(Math.max(0, 1 - factor))
-  const scale = $derived(rm.matches || 1 + 0.1 * fac_inv)
-  sfactor(x => (factor = x))
-
-  let innerWidth = $state(0)
-  let innerHeight = $state(0)
-  const mouse = $state({ x: 0, y: 0 })
-  const mouse_rel = $derived({
-    x: +rm.matches || (1 - (6 * mouse.x) / innerWidth) * fac_inv,
-    y: +rm.matches || (1 - (6 * mouse.y) / innerHeight) * fac_inv,
-  })
-
-  $effect(() => {
-    mouse.x = innerWidth / 2
-    mouse.y = innerHeight / 2
-  })
+  let { scale, splash_rel, ...props } = $props()
 </script>
-
-<svelte:window
-  onmousemove={({ clientX, clientY }) => {
-    mouse.x = clientX
-    mouse.y = clientY
-  }}
-  bind:innerWidth
-  bind:innerHeight
-/>
 
 <svg
   style:--un-scale-x={scale}
   style:--un-scale-y={scale}
-  style:--t-x="{mouse_rel.x}%"
-  style:--un-translate-y="{mouse_rel.y}%"
+  style:--t-x="{splash_rel.x}%"
+  style:--un-translate-y="{splash_rel.y}%"
   viewBox="0 0 1587 1080"
   {...props}
 >
