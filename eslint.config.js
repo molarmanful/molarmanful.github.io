00972950ac8da0eae1a4daf51dab05e405e27dbc
molarmanful <benjinpang@gmail.com>
2024-08-28 11:43:34 -0400

    rework eslint flow

--- eslint.config.js
+++ eslint.config.js
@@ -1,101 +1,10 @@
-import js from '@eslint/js'
-import unocss from '@unocss/eslint-config/flat'
-import prettier from 'eslint-config-prettier'
-import imprt from 'eslint-plugin-import'
-import svelte from 'eslint-plugin-svelte'
-import globals from 'globals'
-import svelteParser from 'svelte-eslint-parser'
+import antfu from '@antfu/eslint-config'
 
-export default [
-  {
-    ignores: ['.svelte-kit/**/*', 'build/**/*'],
+export default antfu({
+  svelte: true,
+  unocss: true,
+}, {
+  rules: {
+    'style/top-level-functions': 'off',
   },
-
-  js.configs.recommended,
-
-  {
-    languageOptions: {
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-  },
-
-  ...svelte.configs['flat/prettier'],
-
-  {
-    files: ['**/*.svelte', '**/*.svelte.js'],
-    languageOptions: {
-      parser: svelteParser,
-    },
-    plugins: {
-      svelte,
-    },
-    rules: {
-      'svelte/sort-attributes': 1,
-    },
-  },
-
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.browser,
-      },
-    },
-    plugins: {
-      import: imprt,
-    },
-    settings: {
-      'import/parsers': {
-        espree: ['.js'],
-      },
-    },
-    rules: {
-      'import/export': 2,
-      'import/no-duplicates': 1,
-      'import/order': [
-        1,
-        {
-          alphabetize: {
-            order: 'asc',
-            caseInsensitive: true,
-          },
-          'newlines-between': 'always',
-        },
-      ],
-      'sort-imports': [
-        1,
-        {
-          ignoreDeclarationSort: true,
-        },
-      ],
-    },
-  },
-
-  {
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        ...globals.node,
-        ...globals.browser,
-      },
-    },
-    rules: {
-      'no-unused-vars': [
-        1,
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
-        },
-      ],
-    },
-  },
-
-  unocss,
-
-  prettier,
-]
+})
