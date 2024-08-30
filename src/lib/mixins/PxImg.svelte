e0ef9396d3dbf543d162d7b0cd5f7ab1e5599c4e
molarmanful <benjinpang@gmail.com>
2024-08-30 14:42:31 -0400

    cleaner

--- src/lib/mixins/PxImg.svelte
+++ src/lib/mixins/PxImg.svelte
@@ -8,23 +8,20 @@
 
   let el: HTMLImageElement | undefined = $state()
   let loaded = $state(false)
+  let w = $state(1)
+  let pw = $state(1)
+  const px = $derived(pw >= w)
 
-  let stop = false
+  let to: ReturnType<typeof setTimeout> | undefined
   const rsz = () => {
-    if (!el || stop)
-      return
-    stop = true
-
-    const w = el.naturalWidth
-    const pw = el.parentElement?.clientWidth || 0
-    el.style.width = `${w * ~~Math.max(1, pw / w)}px`
-    el.style.imageRendering = 'initial'
-    if (pw >= w) {
-      el.style.imageRendering = 'pixelated'
-    }
-
+    clearTimeout(to)
+    setTimeout(() => {
+      if (!el)
+        return
+      w = el.naturalWidth
+      pw = el.parentElement?.clientWidth || 0
+    }, 100)
     loaded = true
-    setTimeout(() => stop = false, 50)
   }
 
   useEventListener(() => window, 'resize', rsz)
@@ -33,6 +30,8 @@
 <div class='w-full'>
   <img
     bind:this={el}
+    style:width='{w * ~~Math.max(1, pw / w)}px'
+    style:image-rendering={px ? 'pixelated' : ''}
     class="{loaded ? 'opacity-100' : 'opacity-0'} block mx-auto mt-6 md:mt-8' max-w-full ofade-200"
     decoding='async'
     loading='lazy'
