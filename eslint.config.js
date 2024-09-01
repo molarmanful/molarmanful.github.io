01142b24680c577c6020d1044a6b861cf7b9e82d
Benjamin Pang (molarmanful) <benjinpang@gmail.com>
2024-09-01 15:10:28 -0400

    prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,29 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  stylistic: true,
-  svelte: {
-    overrides: {
-      'svelte/sort-attributes': 1,
+export default antfu(
+  {
+    stylistic: true,
+    svelte: {
+      overrides: {
+        'svelte/sort-attributes': 1,
+      },
+    },
+    unocss: true,
+    formatters: {
+      css: true,
+      html: true,
     },
   },
-  unocss: true,
-  css: true,
-}, {
-  rules: {
-    'antfu/top-level-function': 0,
-    'import/order': [1, {
-      'alphabetize': {
-        order: 'asc',
-        caseInsensitive: true,
-      },
-      'newlines-between': 'always',
-    }],
+  {
+    rules: {
+      'antfu/top-level-function': 0,
+      'import/order': [1, {
+        'alphabetize': {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      }],
+    },
   },
-})
+)
