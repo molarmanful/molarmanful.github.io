ae5cccd00e0656638f116dadda09253e9eb0fb54
Benjamin Pang (molarmanful) <benjinpang@gmail.com>
2024-09-10 04:39:46 -0400

    bug

--- src/lib/components/ItemBody.svelte
+++ src/lib/components/ItemBody.svelte
@@ -5,9 +5,10 @@
   interface Props extends HTMLAttributes<HTMLDivElement> {
     el?: HTMLElement
     children: Snippet
+    id?: string
   }
 
-  let { el = $bindable(), children, ...rest }: Props = $props()
+  let { el = $bindable(), id, children, ...rest }: Props = $props()
 </script>
 
 <div
@@ -17,6 +18,7 @@
   <!-- svelte-ignore a11y_autofocus -->
   <article
     bind:this={el}
+    {id}
     class='full overflow-x-hidden b-(1 t-0 bord) pb-4 outline-none lg:pb-0 scroll'
     autofocus
     tabindex='-1'
