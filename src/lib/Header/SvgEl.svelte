b225ab18d2be5444f7154eb0e63256cc30a06a74
molarmanful <benjinpang@gmail.com>
2024-09-12 15:34:47 -0400

    splash -> gsap

--- src/lib/Header/SvgEl.svelte
+++ src/lib/Header/SvgEl.svelte
@@ -1,51 +1,42 @@
 <svelte:options namespace='svg' />
 
 <script lang='ts'>
-  import { cD } from '$lib/js/contexts'
+  import gsap from 'gsap'
+
   import type { El } from '$lib/js/me'
-  import { colors } from '$lib/js/static'
+  import { hexes } from '$lib/js/static'
 
   interface Props {
     el: El
   }
 
   const { el, ...rest }: Props = $props()
-  const { loaded } = cD.get()
 
-  const cs = colors[500]
-  const rt = (a = 3000, b = 1000) => 0 | (Math.random() * a + b)
+  const anim = (node: Element) => {
+    gsap.set(node, {
+      color: '#000',
+      strokeDasharray: el.len,
+      strokeDashoffset: el.len,
+    })
 
-  let c = $state('text-black')
-  let t = $state(0)
-  let len = $state(el.len)
-
-  let to: ReturnType<typeof setTimeout> | undefined
-  const loop = () => {
-    t = rt()
-    c = cs[0 | (Math.random() * cs.length)]
-    to = setTimeout(loop, t)
+    const f = (delay: number) => () => {
+      gsap.to(node, {
+        strokeDashoffset: 0,
+        delay,
+        color: gsap.utils.random(hexes[500]),
+        duration: gsap.utils.random(1, 4),
+        ease: delay > 0 ? 'ease-out' : 'ease',
+        onComplete: f(0),
+      })
+    }
+    f(1)()
   }
-
-  $effect(() => {
-    if (!loaded.x)
-      return
-
-    to = setTimeout(() => {
-      len = 0
-      loop()
-    }, 1000)
-
-    return () => clearTimeout(to)
-  })
 </script>
 
 <svelte:element
   this={el.this}
-  style:transition='color, stroke-dashoffset ease-out'
-  style:transition-duration='{t}ms'
-  style:stroke-dasharray={el.len}
-  style:stroke-dashoffset={len}
-  class='{c} fill-transparent stroke-1 stroke-current'
+  class='fill-transparent stroke-1 stroke-current text-black'
+  use:anim
   {...el.attr}
   {...rest}
 />
