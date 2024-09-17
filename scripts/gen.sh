fac049672b8c8e6bcc63798e9c85d6db46bb8823
Benjamin Pang (molarmanful) <benjinpang@gmail.com>
2024-09-17 01:19:09 -0400

    scripts

---
+++ scripts/gen.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+cores="$(nproc --all)"
+
+gen() {
+  local file="${2##*/}"
+  local name="${file%%.*}"
+  node scripts/pics "$2" "out/$1@$name" "$cores"
+}
+
+mkdir -p out
+
+if [ $# -gt 0 ]; then
+  gen "$1" src/lib/"$1/$2"
+else
+  for x in media art covers; do
+    for f in src/lib/"$x"/*; do
+      gen "$x" "$f"
+    done
+  done
+fi
---
+++ scripts/pics.js
@@ -0,0 +1,49 @@
+import path from 'node:path'
+import process from 'node:process'
+
+import sharp from 'sharp'
+
+import { ws } from '../src/common/ws.js'
+
+const [file, out, n] = process.argv.slice(2)
+process.env.UV_THREADPOOL_SIZE = n
+
+const img = sharp(file)
+
+const q = []
+const q_a = []
+
+for (const w of ws) {
+  console.log(`?\t${out} ${w}`)
+
+  const rsz = img.resize({
+    w,
+    fit: 'inside',
+    withoutEnlargement: true,
+  })
+
+  q.push(
+    rsz.clone().jpeg().toFile(path.resolve(`${out}@${w}.jpg`)),
+    rsz.clone().webp().toFile(path.resolve(`${out}@${w}.webp`)),
+  )
+  q_a.push(
+    (x => () =>
+      x.toFile(path.resolve(`${out}@${w}.avif`))
+    )(rsz.clone().avif()),
+  )
+}
+
+try {
+  await Promise.all(q)
+  console.log(`\t,\t${out} jpg,webp`)
+  let i = 0
+  for (const a of q_a) {
+    await a()
+    i++
+    console.log(`\t,\t${out} avif ${i}/${q_a.length}`)
+  }
+  console.log(`.\t${out}`)
+}
+catch (e) {
+  console.error(e)
+}
