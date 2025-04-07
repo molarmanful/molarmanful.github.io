a8ce9a8dcfd789daf6c2309ead9945f5516bb5aa
molarmanful <ben@benpa.ng>
2025-04-06 22:52:47 -0400

    fix: revert unnecessary changes

--- util/pics.js
+++ util/pics.js
@@ -1,7 +1,5 @@
 /* eslint no-console: 0, antfu/no-top-level-await: 0 */
 
-import crypto from 'node:crypto'
-import fs from 'node:fs/promises'
 import path from 'node:path'
 import process from 'node:process'
 
@@ -9,23 +7,13 @@ import sharp from 'sharp'
 
 import { ws } from '../src/common/meta.js'
 
-const [file, out, n, shas] = process.argv.slice(2)
+const [file, out, n] = process.argv.slice(2)
 process.env.UV_THREADPOOL_SIZE = n
 
 const img = sharp(file)
 
 const q = []
 const q_a = []
-
-const mkhash = async (bufp, out) => {
-  const buf = await bufp
-  const hash = crypto.createHash('sha256')
-  hash.update(buf)
-  await Promise.all([
-    fs.appendFile(path.resolve(shas), `${JSON.stringify(out)}=${JSON.stringify(hash.digest('base64'))}\n`),
-    fs.writeFile(path.resolve(out), buf),
-  ])
-}
 
 for (const w of ws) {
   console.log(`?\t${out} ${w}`)
@@ -37,12 +25,12 @@ for (const w of ws) {
   })
 
   q.push(
-    mkhash(rsz.clone().jpeg().toBuffer(), `${out}@${w}.jpg`),
-    mkhash(rsz.clone().webp().toBuffer(), `${out}@${w}.webp`),
+    rsz.clone().jpeg().toFile(path.resolve(`${out}@${w}.jpg`)),
+    rsz.clone().webp().toFile(path.resolve(`${out}@${w}.webp`)),
   )
   q_a.push(
     (x => () =>
-      mkhash(x.toBuffer(), `${out}@${w}.avif`)
+      x.toFile(path.resolve(`${out}@${w}.avif`))
     )(rsz.clone().avif()),
   )
 }
