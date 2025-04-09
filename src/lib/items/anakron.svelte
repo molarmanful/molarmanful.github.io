c39f88196df9d5e0637ae08ddf4728f783ae88ed
molarmanful <ben@benpa.ng>
2025-04-08 22:33:39 -0400

    feat: add anakron

---
+++ src/lib/items/anakron.svelte
@@ -0,0 +1,24 @@
+<script lang='ts' module>
+  export const desc = '2025. A thicc retrofuturistic bitmap font made for the modern screen.'
+  export const tags = ['typography', 'design']
+  export const year = 2025
+</script>
+
+<script lang='ts'>
+  import { A, Heading, Prose, PxImg, Subheading, Text } from '$lib/components'
+</script>
+
+<Prose>
+  <Heading>ANAKRON</Heading>
+  <Subheading>{desc}</Subheading>
+  <Text><A href='https://github.com/molarmanful/ANAKRON'>Source</A></Text>
+</Prose>
+
+<PxImg
+  alt='Sample of the ANAKRON font.'
+  src='https://github.com/molarmanful/ANAKRON/raw/main/img/all.png'
+/>
+<PxImg
+  alt='All glyphs in the ANAKRON font.'
+  src='https://github.com/molarmanful/ANAKRON/raw/main/img/chars.png'
+/>
