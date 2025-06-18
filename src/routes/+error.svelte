dc0ccab26999eaa833f81e9916be5dd3252a5a53
molarmanful <ben@benpa.ng>
2025-06-18 16:31:33 -0400

    feat: error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,3 +5,7 @@
 <svelte:head>
   <title>ERR: {page.status}</title>
 </svelte:head>
+
+<div class='flex items-center inset-0 justify-center absolute'>
+  <h1 class='text-[50vmin] text-accent'>{page.status}</h1>
+</div>
