50c8bbb3521828bca2b7b7f695bdf7fbdbf27202
molarmanful <ben@benpa.ng>
2025-08-18 13:26:16 -0700

    chore: update deps

--- flake.nix
+++ flake.nix
@@ -14,32 +14,34 @@
       systems = import inputs.systems;
       perSystem =
         { pkgs, self', ... }:
-        let
-          unocss-language-server = pkgs.callPackage ./unocss-language-server.nix { };
-        in
         {
           devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              nodejs_latest
-              pnpm
-              nushell
-              # lsps
-              nil
-              nixd
-              vscode-langservers-extracted
-              vtsls
-              svelte-language-server
-              emmet-language-server
-              unocss-language-server
-              stylelint-lsp
-              # formatters
-              nixfmt-rfc-style
-              dprint
-              # linters
-              statix
-              deadnix
-              eslint
-            ];
+            packages =
+              let
+                unocss-language-server = pkgs.callPackage ./unocss-language-server.nix { };
+              in
+              with pkgs;
+              [
+                nodejs_latest
+                pnpm
+                nushell
+                # lsps
+                nil
+                nixd
+                vscode-langservers-extracted
+                vtsls
+                svelte-language-server
+                emmet-language-server
+                unocss-language-server
+                stylelint-lsp
+                # formatters
+                nixfmt-rfc-style
+                dprint
+                # linters
+                statix
+                deadnix
+                eslint
+              ];
           };
 
           formatter = pkgs.writeShellApplication {
